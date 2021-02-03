3b85297507d4bf53ea78737df0f3feff3cee7535
Михаил <makolin@bk.ru>
2021-02-03 19:17:40 +0500

    Добавление даты рождения

--- Phone Book/Model/ApplicationContext.cs
+++ Phone Book/Model/ApplicationContext.cs
@@ -79,6 +79,7 @@ namespace Phone_Book
         public int UserId { get; set; }
         [Required]
         public string Name { get; set; }
+        public DateTime BirtheyDay { get; set; }
         public bool Common { get; set; }
 
         public int? PositionId { get; set; }
@@ -189,6 +190,7 @@ namespace Phone_Book
                     {
                         UserId = 1,
                         Name = "Петров Петр Петрович",
+                        BirtheyDay = new DateTime(1992,12,24),
                         DepartmentId = 1,
                         PositionId = 1,
                         LocalId = 1,
@@ -200,6 +202,7 @@ namespace Phone_Book
                     {
                         UserId = 2,
                         Name = "Иванов Иван Иванович",
+                        BirtheyDay = new DateTime(1992,11,05),
                         DepartmentId = 2,
                         PositionId = 2,
                         LocalId = 2
